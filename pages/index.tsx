fbc55a34352808ea1a5d55ab37bfbb4bbbb8472d
Phil Shields <phil.shields92@outlook.com>
2023-08-11 00:52:05 +0100

    created a sidebar menu with twitter logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
-  <div className="text-3xl text-sky-300">
-      Hello World
-    </div>
+  <div className="text-3xl text-sky-300"> 
+  Hello World</div>
   )
 }
